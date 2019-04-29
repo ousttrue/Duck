ce1c3432fcc6330aa52b2393f991f9cab0f78a37
ousttrue <ousttrue@gmail.com>
2019-04-29 15:51:45 +0900

    add wrap subparser

---
+++ plugin/workspacefolder.vim
@@ -0,0 +1,5 @@
+if exists('g:loaded_workspacefolder')
+    "finish
+endif
+let g:loaded_workspacefolder = 1
+
--- workspacefolder/__init__.py
+++ workspacefolder/__init__.py
@@ -1,2 +1,24 @@
+import argparse
+
 VERSION = [0, 1]
 
+
+def main():
+    parser = argparse.ArgumentParser(description='WorkspaceFolder tool.')
+
+    sub = parser.add_subparsers(title='action')
+
+    wrap = sub.add_parser('wrap')
+    wrap.set_defaults(action='wrap')
+    wrap.add_argument('cmd', type=str, nargs='+', help='''cmd and arguments''')
+
+    args = parser.parse_args()
+
+    if args.action == 'wrap':
+        print(args.cmd)
+    else:
+        raise ValueError(args.action)
+
+
+if __name__ == '__main__':
+    main()
